065e15a657e2b0214fbd41dcf5704e329d80f27c
hweiyu2002 <hweiyu1001@gmail.com>
2022-04-29 14:54:20 +0800

    examing

---
+++ src/com/fju/ai/Box1.java
@@ -0,0 +1,9 @@
+package com.fju.ai;
+
+public class Box1 extends Box{
+    public Box1(int length, int width, int height){
+        super(length, width, height);
+
+    }
+
+}
---
+++ src/com/fju/ai/Box2.java
@@ -0,0 +1,4 @@
+package com.fju.ai;
+
+public class Box2 {
+}
